f13cca451fda36701c10d47c463eccbf11d9981b
D064647 <carsten.loeher@sap.com>
2020-07-08 15:05:41 +0200

    Added windows start script

---
+++ run_windows_local.ps1
@@ -0,0 +1,14 @@
+# Local Application Start
+
+## Start backend
+### Set port
+$Env:PORT = "8080"
+### Start API
+invoke-expression 'cmd /c start powershell -Command {write-host "Start empathygame api server"; py ./api/main.py }'
+
+
+## Start @SAP/Approuter
+### Set port
+$Env:PORT = "3000"
+### Start @SAP/Approuter
+invoke-expression 'cmd /c start powershell -Command {write-host "Start empathygame approuter"; npm run dev --prefix ./web}'
