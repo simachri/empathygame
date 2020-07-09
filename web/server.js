92a7f579e43336e1852adfc799a4e762363ac213
D064647 <carsten.loeher@sap.com>
2020-07-09 12:32:15 +0200

    Manual route adjustment

--- web/server.js
+++ web/server.js
@@ -1,8 +1,13 @@
 var AppRouter = require("@sap/approuter");
 
+
+//--Define routes
+process.env.destinations = '[' +
+'{"name": "api","url": "https://empathygame-api.cfapps.eu10.hana.ondemand.com"}' +
+']'; //--Route to local python backend
+
 var approuter = new AppRouter();
 var options = {
-    xsappConfig: xsAppConfig,
     port: process.env.PORT || 3000
 };
 approuter.start(options);
