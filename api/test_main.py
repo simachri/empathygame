7b676ba7c579876c5c667a1a3db5822db9f11982
simachri <simachri@devnull.de>
2020-08-06 01:33:31 -0500

    feat: Return assigned roles on game join. Closes #3.

--- api/test_main.py
+++ api/test_main.py
@@ -2,7 +2,7 @@ import os
 import time
 import unittest
 from multiprocessing import Process
-from typing import List
+from typing import List, Dict
 
 import socketio
 # Set the port as environment variable as it is pulled when starting the uvicorn server.
@@ -10,8 +10,11 @@ import socketio
 import uvicorn
 from socketio import Client
 
-from events import NEW_GAME, GAME_JOINED, JOIN_GAME
+from events import NEW_GAME, GAME_JOINED, JOIN_GAME, ROLES_ASSIGNED, ASSIGN_ROLES
 from models import SioNewGame, Game, Player, Scenario
+
+CLIENT1_USER_NAME = 'Player 1'
+CLIENT2_USER_NAME = 'Player2'
 
 os.environ['PORT'] = '8080'
 from api.main import SOCKETIO_PATH
@@ -42,7 +45,7 @@ def start_server():
     server_proc = Process(target=launch_uvicorn, args=(), daemon=True)
     server_proc.start()
     # Wait until the server has started. I know this is dirty.
-    time.sleep(2)
+    time.sleep(1)
 
 
 def stop_server():
@@ -62,7 +65,7 @@ class TestSocket(unittest.TestCase):
 
     def __init__(self, method: str = ...) -> None:
         super().__init__(method)
-        self.new_game_model: SioNewGame = SioNewGame(user_name='Player Unittest', game_scenario='42')
+        self.new_game_model: SioNewGame = SioNewGame(user_name=CLIENT1_USER_NAME, game_scenario='42')
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -82,19 +85,33 @@ class TestSocket(unittest.TestCase):
         self.client2 = socketio.Client()
         self.client2.connect('http://localhost:8080', transports='websocket', socketio_path=SOCKETIO_PATH)
         # Wait a second until the connection has been established.
-        time.sleep(1)
+        time.sleep(0.3)
 
     def tearDown(self) -> None:
         """Test setup; is called before every unit test"""
         self.client1.disconnect()
+        self.client2.disconnect()
         self.new_game_model.game = None
+        time.sleep(0.3)
 
     def test_on_game_joined_assigned_roles_sent(self):
-        """Test that if the client receives the event that he/she has successfully joined a game, the payload
-           of that event contains the assigned roles for each players.
+        """Test that if the client receives the event that he/she has successfully joined a game, the client
+           also receives the 'roles_assigned' event (if the roles have already been assigned for that specific game.
            Tests issue #3."""
+        # Let the first player (host) create a game.
         game: Game = self.create_game()
-        self.join_game(self.client2, 'Player2', game.id, game.pwd)
+        # Let the second player join the game.
+        self.join_game(self.client2, CLIENT2_USER_NAME, game.id, game.pwd)
+        # Let the host assign the roles.
+        self.assign_roles(self.client1)
+        # Let the second player join the game again.
+        players: List[Dict] = self.join_game(self.client2, CLIENT2_USER_NAME, game.id, game.pwd)
+        # Make sure that the role assignment information is available.
+        for p in players:
+            # noinspection PyUnresolvedReferences
+            self.assertIsNotNone(p['role_name'], "Role assignment is missing on game rejoin.")
+            self.assertIsNotNone(p['role_id'], "Role assignment is missing on game rejoin.")
+            self.assertIsNotNone(p['role_descr'], "Role assignment is missing on game rejoin.")
 
     def create_game(self) -> Game:
         """Create a new game for scenario 42.
@@ -122,15 +139,15 @@ class TestSocket(unittest.TestCase):
         return self.new_game_model.game
 
     @staticmethod
-    def join_game(client: Client, user_name, game_id, game_pwd) -> List[Player]:
+    def join_game(client: Client, user_name, game_id, game_pwd) -> List[Dict]:
         """Join an existing game.
         :param client: SocketIO client
         :param user_name: Name of the user to join the game
         :param game_id: Game ID
         :param game_pwd: Game password
-        :return: SioJoinGame.Game.players
+        :return: The 'players' result of SioJoinGame.emit()
         """
-        players: List[Player] = []
+        players: List[Dict] = []
 
         # Set the handler when the server responds after the player has joined the game.
         def on_game_joined(data):
@@ -143,3 +160,23 @@ class TestSocket(unittest.TestCase):
         while not players:
             time.sleep(1)
         return players
+
+    @staticmethod
+    def assign_roles(client: Client) -> List[Dict[str, str]]:
+        """Triggers the role assignment.
+        :param client: Client that triggers the role assignment.
+        :return: List of dicts with data of the players and their roles.
+        """
+        roles: List[Dict[str, str]] = []
+
+        # Set the handler when the server responds after the roles have been assigned.
+        def on_roles_assigned(data):
+            nonlocal roles
+            roles = data['players']
+
+        client.on(ROLES_ASSIGNED, on_roles_assigned)
+        client.emit(ASSIGN_ROLES, {})
+        # Wait until on_roles_assigned has been called.
+        while not roles:
+            time.sleep(1)
+        return roles
